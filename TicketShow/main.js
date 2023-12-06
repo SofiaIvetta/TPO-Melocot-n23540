245cc7984d316ee1e3bd71e2da3bb11e548ec2d9
SofiaIvetta <145399725+SofiaIvetta@users.noreply.github.com>
2023-12-05 22:27:07 -0300

    Delete TicketShow/main.js

--- TicketShow/main.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("header").innerHTML=` <nav class="navbar navbar-expand-sm
-navbar-light bg-light">
-<div class="container">
-<a class="navbar-brand" href="../templates/productos.html">CRUD</a>
-
-
-
-</div>
-</nav>`
