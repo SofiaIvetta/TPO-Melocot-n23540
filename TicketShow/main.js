4894980d963081b8a640ee84c5287901749c5128
SofiaIvetta <145399725+SofiaIvetta@users.noreply.github.com>
2023-12-05 22:24:48 -0300

    Add files via upload

---
+++ TicketShow/main.js
@@ -0,0 +1,9 @@
+document.getElementById("header").innerHTML=` <nav class="navbar navbar-expand-sm
+navbar-light bg-light">
+<div class="container">
+<a class="navbar-brand" href="../templates/productos.html">CRUD</a>
+
+
+
+</div>
+</nav>`
